245651b837ca94b227128d14998f105deefd6ae8
LendyAbel <lendyabel93@gmail.com>
2025-05-15 13:00:51 +0200

    blog_list_backend Step 2 Refactoring controller and app

--- blog_list_backend/index.js
+++ blog_list_backend/index.js
@@ -1,31 +1,4 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const Blog = require('./models/blog_model')
-
-const app = express()
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl).then(res => {
-  console.log('Connected to MongoDB')
-})
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then(result => {
-    console.log('POST done')
-    response.status(201).json(result)
-  })
-})
+const app = require('./app')
 
 const PORT = 3003
 app.listen(PORT, () => {
