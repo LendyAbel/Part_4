6e4ab258a5a85165cdced6c968ef5d151807a4ab
LendyAbel <lendyabel93@gmail.com>
2025-05-26 13:33:17 +0200

    4.17 Expand blogs so that each blog contains information on the creator of the blog

--- blog_list_backend/controllers/blog_controller.js
+++ blog_list_backend/controllers/blog_controller.js
@@ -1,25 +1,35 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog_model')
+const User = require('../models/user_model')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {
+    blogs: 0,
+  })
   response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
+  if (!request.body.title || !request.body.url) {
+    return response.status(400).end()
+  }
   if (!request.body.likes) {
     request.body.likes = 0
   }
 
-  if (!request.body.title || !request.body.url) {
-    response.status(400).end()
-  } else {
-    const newBlog = new Blog(request.body)
-
-    const savedBlog = await newBlog.save()
-    console.log('POST done')
-    response.status(201).json(savedBlog)
+  const { title, author, url, likes, userId } = request.body
+  const user = await User.findById(userId)
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
   }
+
+  const newBlog = new Blog({ title, author, url, likes, user: user.id })
+
+  const savedBlog = await newBlog.save()
+  console.log('POST done')
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.status(201).json(savedBlog)
 })
 
 blogRouter.delete('/:id', async (request, response) => {
