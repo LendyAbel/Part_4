2dbbbc0e3500c9a5d0d1469ae47614d9e4f8f601
LendyAbel <lendyabel93@gmail.com>
2025-06-09 11:51:07 +0200

    5.7 Adding button to control visibility of the full block or short blog

--- blog_list_frontend/src/components/Blog.jsx
+++ blog_list_frontend/src/components/Blog.jsx
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const fullBlog = () => {
+    return (
+      <div>
+        <p>Title: {blog.title}</p>
+        <p>URL: {blog.url}</p>
+        <p>
+          likes: {blog.likes} <button>like</button>
+        </p>
+        <p>Author: {blog.author}</p>
+      </div>
+    )
+  }
+  const shortBlog = () => {
+    return (
+      <p>
+        Title: {blog.title} Author: {blog.author}
+      </p>
+    )
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      {visible ? fullBlog() : shortBlog()}{' '}
+      <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+    </div>
+  )
+}
 
 export default Blog
