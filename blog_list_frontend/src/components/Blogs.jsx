63d6fb641acae0f5d00edde991d13b8421c92392
LendyAbel <lendyabel93@gmail.com>
2025-06-10 11:52:59 +0200

    Adding delete funcionality

--- blog_list_frontend/src/components/Blogs.jsx
+++ blog_list_frontend/src/components/Blogs.jsx
@@ -1,13 +1,13 @@
 import Blog from './Blog'
 
-const Blogs = ({ blogs, updateLikes }) => {
+const Blogs = ({ blogs, updateLikes, deleteBlog }) => {
   const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes)
 
   return (
     <div>
       <h2 className='subtitle'>Blogs</h2>
       {sortedBlogs.map(blog => (
-        <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
+        <Blog key={blog.id} blog={blog} updateLikes={updateLikes} deleteBlog={deleteBlog} />
       ))}
     </div>
   )
