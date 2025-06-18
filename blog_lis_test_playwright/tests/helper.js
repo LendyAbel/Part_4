630162dab3a88a5a7fe977d0f266565a955bee16
LendyAbel <lendyabel93@gmail.com>
2025-06-18 12:50:13 +0200

    5.19 Adding test to check if a new blog cant be created

---
+++ blog_lis_test_playwright/tests/helper.js
@@ -0,0 +1,15 @@
+const login = async (page, username, password) => {
+  await page.locator('input[name="Username"]').fill(username)
+  await page.locator('input[name="Password"]').fill(password)
+  await page.getByRole('button', { name: 'login' }).click()
+}
+
+const createBlog = async (page, title, author, url) => {
+  await page.getByRole('button', { name: 'Create new blog' }).click()
+  await page.locator('input[name="title"]').fill(title)
+  await page.locator('input[name="author"]').fill(author)
+  await page.locator('input[name="url"]').fill(url)
+  await page.getByRole('button', { name: 'Create' }).click()
+}
+
+export { login, createBlog }
